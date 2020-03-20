ed72528ae13cb0eb4c82c11fc2143486263c1e1f
theophile <theophilevonck@gmail.com>
2020-03-20 11:46:00 +0100

    load wav file

---
+++ load_file.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+
+def load( nomfichier ):
+    folder="Audio_files/"
+    extention=".wav"
+    path=folder+nomfichier+extention
+    spf = wave.open(path, "r")
+
+    # Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, "Int16")
+
+
+    # If Stereo
+    if spf.getnchannels() == 2:
+        print("Just mono files")
+        sys.exit(0)
+
+    plt.figure(1)
+    plt.title("Signal Wave...")
+    plt.plot(signal)
+    plt.show()
+
+load("son")
