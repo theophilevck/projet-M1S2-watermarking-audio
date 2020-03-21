90343c68b33a7837356dbeba7c6bdca7cbf9b0da
Antoine Versmessen <antoine@versmessen.fr>
2020-03-21 18:26:07 +0100

    Spectrology logo isen

--- convert_image_2_audio.py
+++ convert_image_2_audio.py
@@ -1,3 +1,36 @@
-from PIL import Image
-im = Image.open("Image_files\eye.png")
-im.rotate(45).show()
+from PIL import Image, ImageOps
+import wave, math, array, argparse, sys, timeit
+
+minfreq = 200
+maxfreq = 20000
+wavrate = 44100
+pxs     = 30
+
+#im = Image.open("Image_files\eye.png")
+#im.rotate(45).show()
+
+extension=".bmp"
+img = Image.open("Image_files\eye"+extension).convert('L')
+img.show()
+
+a=input("Voulez-vous faire une rotation de l'image ? yes-no  ")
+if a==("yes"):
+    img=img.rotate(90)
+    img.show()
+
+b=input("Voulez-vous mettre l'image en nuances de noir et blanc ? yes-no  ")
+if b==("yes"):
+    img = ImageOps.invert(img)
+    img.show()
+
+
+extension=".wav"
+output = wave.open("Image_files\eye"+extension, 'w')
+
+intervalfreq = maxfreq - minfreq
+interval = intervalfreq / img.size[1]
+
+fpx = wavrate // pxs     # Hz // px
+data = array.array('h')  # array de int
+
+
