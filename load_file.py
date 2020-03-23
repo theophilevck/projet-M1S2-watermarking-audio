1c8c86a911898620d963702cdaf623392ba5a5bd
theophile <theophilevonck@gmail.com>
2020-03-23 11:41:33 +0100

    debut ftt

--- load_file.py
+++ load_file.py
@@ -3,7 +3,8 @@ import numpy as np
 import wave
 import sys
 
-def load( nomfichier: str, nomfichierwatermark: str ):
+
+def loadcompare( nomfichier: str, nomfichierwatermark: str ):
     folder="Audio_files/"
     extention=".wav"
     path1=folder+nomfichier+extention
@@ -20,32 +21,83 @@ def load( nomfichier: str, nomfichierwatermark: str ):
     signal2 = spf2.readframes(-1)
     signal2 = np.fromstring(signal2, "Int16")
         
-
     Time = np.linspace(0, len(signal1) / fs, num=len(signal1))
-
-   
-
+    
     figure = plt.figure(figsize = (10, 5))
     plt.gcf().subplots_adjust(left = 0.1, bottom = 0.1,
-                           right = 0.9, top = 0.9, wspace = 0, hspace = 1)
+                           right = 0.9, top = 0.9, wspace = 0, hspace = 0.5)
     axes = figure.add_subplot(2, 1, 1)
     axes.set_xlabel('Time (s)')
     axes.set_ylabel('Frequence(Hz)')
-    axes.set_title('titre du graphe 1')
+    axes.set_title('without watermark')
     plt.plot(Time, signal1)
     
     axes = figure.add_subplot(2, 1, 2)
     axes.set_xlabel('Time (s)')
     axes.set_ylabel(' Frequence(Hz)')
-    axes.set_title('titre du graphe 2')
+    axes.set_title('with watermark')
     plt.plot(Time, signal2)
 
     plt.show()
 
     
+def load( nomfichier: str ):
+    folder="Audio_files/"
+    extention=".wav"
+    path1=folder+nomfichier+extention
+
+    spf1 = wave.open(path1, "r")
+    return spf1
+
+def freqsignal (nomfichier: str):
+    spf=load( nomfichier )
+    fs = spf.getframerate()
+
+    # Extract Raw Audio from Wav File
+    signal1 = spf.readframes(-1)
+    signal1 = np.fromstring(signal1, "Int16")
+
+    Time = np.linspace(0, len(signal1) / fs, num=len(signal1))
+
+    plt.figure(figsize = (10, 5))
+    plt.title( nomfichier+' frequence' )
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequence(Hz)')
+    
+    plt.plot(Time, signal1)
+    
+    plt.show()
+
+def FFTsignal (nomfichier: str):
+    spf=load( nomfichier )
+    fs = spf.getframerate()
+    # Extract Raw Audio from Wav File
+    signal1 = spf.readframes(-1)
+    signal1 = np.fromstring(signal1, "Int16")
+    
+
+
+    Time = np.linspace(0, len(signal1) / fs, num=len(signal1))
+
+    fft = np.fft.fft(signal1)
+
+
+    T = Time[1] - Time[0]  # sampling interval 
+    N = signal1.size
+
+    # 1/T = frequency
+    f = np.linspace(0, 1 / T, N)
+
+    plt.figure(figsize = (10, 5))
+    plt.title( nomfichier+' FFT' )
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frequency [Hz]")
+    plt.bar(f[:N // 2], np.abs(fft)[:N // 2] * 1 / N, width=1.5)  # 1 / N is a normalization factor
+    plt.show() 
 
 
     
     
 
-load("wilhelm","wilhelm_watermarked_lsb")
+FFTsignal("wilhelm_watermarked_lsb")
+
